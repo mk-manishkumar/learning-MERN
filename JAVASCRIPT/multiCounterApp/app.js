78771b421190ec6f968ce79630f2f3ebd3a747c0
Manish Kumar <manish.login01@gmail.com>
2023-08-05 20:17:16 +0530

    solving a new coding question in - multiCounterApp

---
+++ JAVASCRIPT/multiCounterApp/app.js
@@ -0,0 +1,6 @@
+const increase = document.querySelector(".increase");
+const decrease = document.querySelector(".decrease");
+const reset = document.querySelector(".reset");
+
+const countValue = document.querySelector("#count-value");
+const countJump = document.querySelector("#count-jump");
