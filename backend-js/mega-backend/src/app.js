260618872e72aec9e0047b786ff909c0f37cb964
Manish Kumar <manish.login01@gmail.com>
2024-04-11 23:02:59 +0530

    learnt about router and controller

--- backend-js/mega-backend/src/app.js
+++ backend-js/mega-backend/src/app.js
@@ -16,4 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "25kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
 export { app };
