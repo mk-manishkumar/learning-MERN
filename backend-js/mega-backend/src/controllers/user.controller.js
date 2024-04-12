e211d841ec47ca2a53b5dd399ffa8c01cd0daec1
Manish Kumar <manish.login01@gmail.com>
2024-04-12 20:03:06 +0530

    wrote logics for user routes and controllers

--- backend-js/mega-backend/src/controllers/user.controller.js
+++ backend-js/mega-backend/src/controllers/user.controller.js
@@ -1,9 +1,34 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "OK",
-  });
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exists through username and email
+  // check for images, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object - create entry in dB
+  // remove  password and refresh token field from response
+  // check for user creation
+  // return res
+
+  const { fullName, username, email, password } = req.body;
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+   const existedUser = await User.findOne({
+     $or: [{ username }, { email }],
+   });
+
+   if (existedUser) {
+     throw new ApiError(409, "User with email or username already exists");
+   }
+
+   
 });
 
 export { registerUser };
